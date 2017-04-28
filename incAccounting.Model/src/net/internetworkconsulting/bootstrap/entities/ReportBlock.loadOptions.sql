55465f2664af7069a24bd907154f28714f97bb97
Shawn Zernik <shawn@internetworkconsulting.net>
2017-04-28 07:27:35 -0500

    Rolled back to https://github.com/InternetworkConsultingLLC/incAccounting/commit/09895184ba905b4c039a6efe0a68d4d7be8efc6f
    Rolled back to
    https://github.com/InternetworkConsultingLLC/incAccounting/commit/09895184ba905b4c039a6efe0a68d4d7be8efc6f

--- incAccounting.Model/src/net/internetworkconsulting/bootstrap/entities/ReportBlock.loadOptions.sql
+++
@@ -1,7 +0,0 @@
-SELECT  
-	"Report Blocks"."Name" AS "Display",
-	"Report Blocks"."GUID" AS "Value"
-FROM 
-	"Report Blocks" 
-ORDER BY
-	"Name"
--- incAccounting.Model/src/net/internetworkconsulting/bootstrap/entities/User.loadOptions.sql
+++
@@ -1,7 +0,0 @@
-SELECT  
-	"Users"."Display Name" AS "Display",
-	"Users"."GUID" AS "Value"
-FROM 
-	"Users" 
-ORDER BY
-	"Display Name"
